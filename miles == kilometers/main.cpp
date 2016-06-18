c6f2829a07d24f2e7c487531ea167962775704ed
nosmilewithme <davittavadze31@gmail.com>
2016-06-18 13:40:33 +0200

    Initial Commit

---
+++ miles == kilometers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  miles == kilometers
+//
+//  Created by Davit Tavadze on 2016-06-18.
+//  Copyright © 2016 starcounter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
